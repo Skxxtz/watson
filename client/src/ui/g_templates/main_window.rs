3b1707278ec4a3e3b23f41bf260333e1d2d31287
Sebastian Kootz <skxxtz@gmail.com>
2026-01-10 01:37:52 +0100

    impr: fixed scrolling

--- client/src/ui/g_templates/main_window.rs
+++ client/src/ui/g_templates/main_window.rs
@@ -2,7 +2,7 @@ mod imp {
     use std::cell::RefCell;
     use std::rc::Rc;
 
-    use gtk4::glib;
+    use gtk4::{ScrolledWindow, glib};
     use gtk4::subclass::prelude::*;
     use gtk4::{ApplicationWindow, Box as GtkBox};
 
@@ -13,6 +13,9 @@ mod imp {
     pub struct MainWindow {
         #[template_child(id = "viewport")]
         pub viewport: TemplateChild<GtkBox>,
+
+        #[template_child(id = "viewport-scroll")]
+        pub viewport_scroll: TemplateChild<ScrolledWindow>,
 
         pub state: Rc<RefCell<UiState>>,
     }
@@ -38,8 +41,10 @@ mod imp {
     impl ApplicationWindowImpl for MainWindow {}
 }
 
+use gtk4::gdk::{Display, Monitor};
 use gtk4::gio::{ActionGroup, ActionMap};
 use gtk4::glib::Object;
+use gtk4::glib::subclass::types::ObjectSubclassIsExt;
 use gtk4::prelude::*;
 
 gtk4::glib::wrapper! {
@@ -55,6 +60,19 @@ impl MainWindow {
         obj.set_application(Some(application));
         obj.set_default_width(width);
         obj.set_opacity(opacity);
+
+        let imp = obj.imp();
+        if let Some(display) = Display::default() {
+            let monitors = display.monitors();
+            if let Some(monitor) = monitors.item(0).and_downcast::<Monitor>() {
+                let geo = monitor.geometry();
+                let max_h = (geo.height() as f32 * 0.9) as i32;
+                imp.viewport_scroll.set_max_content_height(max_h);
+            }
+        }
+
+
         obj
+
     }
 }
